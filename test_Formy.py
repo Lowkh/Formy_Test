82fa686e8e271233477837ce5c6a3e73c1df354c
Lowkh <54532616+Lowkh@users.noreply.github.com>
2020-01-03 22:18:20 +0800

    Update test_Formy.py

--- test_Formy.py
+++ test_Formy.py
@@ -11,7 +11,7 @@ def driver():
     chrome_options.add_argument("--headless") 
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument("--disable-gpu")
-    driver = webdriver.Chrome(chrome_options=chrome_options)      
+    driver = webdriver.Chrome(options =chrome_options)      
     yield driver
     driver.quit()
     # create a object for the chrome driver and pass around
