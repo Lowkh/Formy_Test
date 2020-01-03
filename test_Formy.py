97b2915324657594c20d06da851ad8dbbaf6f775
Lowkh <54532616+Lowkh@users.noreply.github.com>
2020-01-03 22:11:40 +0800

    Update test_Formy.py

--- test_Formy.py
+++ test_Formy.py
@@ -1,12 +1,17 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options  
 import pytest
 import time
 
 @pytest.fixture()
 def driver():
-    driver = webdriver.Chrome()
+    chrome_options = Options()  
+    chrome_options.add_argument("--headless") 
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(chrome_options=chrome_options)      
     yield driver
     driver.quit()
     # create a object for the chrome driver and pass around
