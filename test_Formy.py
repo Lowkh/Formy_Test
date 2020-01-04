1e503daa8e7e32e584b7c7695c042dc2e7bb0045
Lowkh <54532616+Lowkh@users.noreply.github.com>
2020-01-04 10:53:18 +0800

    Update test_Formy.py

--- test_Formy.py
+++ test_Formy.py
@@ -22,7 +22,7 @@ def test_formy_buttons(driver):
     driver.get("https://formy-project.herokuapp.com/buttons")
     elem = driver.find_element_by_css_selector('.btn-primary')
     driver.save_screenshot("Screeny1.png")
-    assert "Secondary" == elem.text
+    assert "Primary" == elem.text
 '''    
     elem = driver.find_element_by_css_selector('.btn-success')
     elem.click()
